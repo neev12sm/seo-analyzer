43af8a6452bd5c075bc1d040f6db1390f3633bf1
neev <neevsharma828@gmail.com>
2026-07-07 12:07:20 +0530

    final touch

--- server/services/crawler.service.js
+++ server/services/crawler.service.js
@@ -2,13 +2,23 @@ const puppeteer = require("puppeteer");
 
 const crawlWebsite = async (url) => {
   const browser = await puppeteer.launch({
-    headless: true
+    headless: true,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-gpu",
+      "--no-first-run",
+      "--no-zygote",
+      "--single-process"
+    ]
   });
 
   const page = await browser.newPage();
 
   await page.goto(url, {
-    waitUntil: "networkidle2"
+    waitUntil: "networkidle2",
+    timeout: 60000
   });
 
   const html = await page.content();
